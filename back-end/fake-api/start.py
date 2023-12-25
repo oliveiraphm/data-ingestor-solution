b79042ab817dabfba4d235761a4a013a1dd55116
oliveiraphm <oliveiraphm@gmail.com>
2023-12-25 16:31:48 -0300

    endpoint varios registros

---
+++ back-end/fake-api/start.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI
+from faker import Faker
+import pandas as pd
+import random 
+
+
+app = FastAPI()
+fake = Faker()
+
+file_name = 'back-end/fake-api/products.csv'
+df = pd.read_csv(file_name, sep=";")
+df['EAN'] = df['EAN'].astype(str)
+df['indice'] = range(1, len(df)+1)
+df.set_index('indice', inplace=True)
+
+@app.get("/gerar_compra")
+async def gerar_compra():
+    index = random.randint(1, len(df)-1)
+    tuple = df.iloc[index]
+
+    return{
+        "client": fake.name(),
+        "creditcard": fake.credit_card_provider(),
+        "product_name": tuple["Produto"],
+        "ean": tuple["EAN"],
+        "price": round(float(tuple["Preco"])*1.2,2),
+        "dateTime": fake.iso8601(),
+        "clientPosition": fake.location_on_land()
+    }
+
+@app.get("/gerar_compra/{numero_registro}")
+async def gerar_compra(numero_registro: int):
+
+    if numero_registro < 1 :
+        return{ "error":  "numero_registro deve ser maior que 1"}
+    
+    respostas = []
+    for _ in range(numero_registro):
+        index = random.randint(1, len(df)-1)
+        tuple = df.iloc[index]
+        compra = {
+            "client": fake.name(),
+            "creditcard": fake.credit_card_provider(),
+            "product_name": tuple["Produto"],
+            "ean": tuple["EAN"],
+            "price": round(float(tuple["Preco"])*1.2,2),
+            "dateTime": fake.iso8601(),
+            "clientPosition": fake.location_on_land()
+        }
+        respostas.append(compra)
+    return respostas
